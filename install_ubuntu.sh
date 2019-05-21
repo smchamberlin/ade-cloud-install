b94cf0aa8dcfb1377774309c2e453bbd26dcd944
smchamberlin <smc2@us.ibm.com>
2019-05-21 14:06:25 -0700

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,7 +1,7 @@
 apt update
 apt-get -y install docker.io
 apt remove -y golang-docker-credential-helpers
-docker login -u iamapikey -p HhfLqrATtQQVemiRY569VKrnaC2VlFYDn32RoK5OGLqb registry.ng.bluemix.net
+docker login -u iamapikey -p <your_API_key> registry.ng.bluemix.net
 apt-get -y install docker-compose
 sysctl -w vm.max_map_count=262144
 wget https://raw.githubusercontent.com/smchamberlin/ade-cloud-install/master/docker-compose.yml
